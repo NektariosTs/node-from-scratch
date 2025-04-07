c0bcd872f8b81d8b2d487ac00ea8bf214c9f0f28
Nektarios Tsagkaris <nektarios.tsagkaris@gmail.com>
2025-04-07 22:05:28 +0300

    apps and explanations for express/mongoose/typescpipt

---
+++ myapp3/app.js
@@ -0,0 +1,51 @@
+//1**.dhlwnoume poia selida theloume na mas anoiksei(form), kai sthn deuterh parametro       pername tis metavlites pou thelw na steilw sthn selida form
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({extended:true}));
+
+app.set("view engine", "ejs");
+
+app.get("/create", (req, res) => {
+  console.log("Create Page");
+  res.render("form", {})//1**
+})
+
+app.post("/user", (req, res) => {
+  console.log("User Post")
+  let data = req.body;
+  console.log("Data", data);
+
+  let username = data.username;
+  let email = data.email;
+
+  res.render("user",
+    {
+      data1: username,
+      data2: email
+    }
+  )
+})
+
+app.get("/users", (req, res) => {
+  console.log("Users Page");
+
+  const users = [
+    {
+      "username": "markos",
+      "email": "marka@aueb.gr"
+    },
+    {
+      "username":"thanasis",
+      "email": "thanasis@aueb.gr"
+    }
+  ]
+
+  res.render("users", {users})
+})
+
+app.listen(port, () => {
+  console.log("Server is up!");
+})
