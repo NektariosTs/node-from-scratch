f6f0e7c3e46e0f842f1b51c5eef3b8410cd6083b
Nektarios Tsagkaris <nektarios.tsagkaris@gmail.com>
2025-04-08 16:13:55 +0300

    usersapp schemas/controllers

--- usersApp/app.js
+++ usersApp/app.js
@@ -2,4 +2,12 @@ const express = require("express");
 const app = express()
 
 
-module.exports = app //exports app
+app.use(express.json());
+
+const user = require("./routes/user.routes");
+
+
+app.use("/api/users", user);
+// app.post("/api/users", user);
+
+module.exports = app //exports app 
--- usersApp/server.js
+++ usersApp/server.js
@@ -1,15 +1,19 @@
+//1* enw prwta ginetai h diadikasia na syndethei sthn vash kai meta ston server,edw ginetai to antitheto giati
+//to promise auto kanei sthn mongoose connect exoume promsise opote kapoia stigmh tha ulopoihthei h sunarthsh alla mexri na ginei auto den stamataei to programma, energopoietai prwta h apo katw diadiasia
+
+
 const mongoose = require("mongoose");
 const app = require("./app")//import app.js
 const port = 3000;
 
-mongoose.connect("")
-.then(
+mongoose.connect(process.env.MONGODB_URI)
+.then(//1*
   () => {
     console.log("Connection to MongoDB established");
+    app.listen(port,() => {
+      console.log("Server is up")
+    })
   },
   err => {console.log("Failed to connect to MongoDB", err); }
 )
 
-app.listen(port,() => {
-  console.log("Server is up")
-})
