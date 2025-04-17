36ec69ac3534f6f9c1b9ec1e58575dc83c2f756f
nektarios.tsagkaris <nektarios.tsagkaris@gmail.com>
2025-04-17 14:15:36 +0300

    jest/test

--- usersApp/routes/auth.routes.js
+++ usersApp/routes/auth.routes.js
@@ -8,4 +8,4 @@ router.get('/google/callback', authController.googleLogin);
 
 module.exports = router
 
-// https://accounts.google.com/o/oauth2/auth?client_id=49291455257-p295u853vt861qredqvkvg24m29req43.apps.googleusercontent.com&redirect_uri=http://localhost:3000/api/auth/google/callback&response_type=code&scope=email%20profile&access_type=offline
+// https://accounts.google.com/o/oauth2/auth?client_id=947710428460-if7sjh0i0i5vn7fqvh5tvq3pacnck4gf.apps.googleusercontent.com&redirect_uri=http://localhost:3000/api/auth/google/callback&response_type=code&scope=email%20profile&access_type=offline
