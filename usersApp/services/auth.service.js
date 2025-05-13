87e2604a8163b2bf55cff31b380487d6d1d02d5d
nektarios.tsagkaris <nektarios.tsagkaris@gmail.com>
2025-05-13 21:25:57 +0300

    coverage

--- usersApp/services/auth.service.js
+++ usersApp/services/auth.service.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const { OAuth2Client } = require('google-auth-library')
 
-function generateAccessToken(user) {
+function generateAccessToken(user){
 
   // console.log("Auth Service", user);
 
@@ -12,14 +12,14 @@ function generateAccessToken(user) {
   }
 
   const secret = process.env.TOKEN_SECRET;
-  const options = { expiresIn: '1h' };
+  const options = { expiresIn: '1h'};
 
   return jwt.sign(payload, secret, options);
 }
 
-function verifyAccessToken(token) {
+function verifyAccessToken(token){
   const secret = process.env.TOKEN_SECRET;
-
+  
   try {
     const payload = jwt.verify(token, secret);
 
@@ -31,29 +31,32 @@ function verifyAccessToken(token) {
 }
 
 async function googleAuth(code) {
-  console.log("Google login");
+  console.log("Google login", code);
   const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
   const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
   const REDIRECT_URI = process.env.REDIRECT_URI;
 
-  const oAuth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+  const oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 
   try {
-    //Exchange code for tokens
-    const { tokens } = await oAuth2Client.getToken(code)
-    oAuth2Client.setCredentials(tokens)
+    // Exchange code for tokens
+    const { tokens } = await oauth2Client.getToken(code)
+    console.log("Step 1", tokens)
+    oauth2Client.setCredentials(tokens)
 
-    const ticket = await oAuth2Client.verifyIdToken({
+    const ticket = await oauth2Client.verifyIdToken({
       idToken: tokens.id_token,
       audience: CLIENT_ID
     });
 
+    console.log("Step 2")
+
     const userInfo = await ticket.getPayload();
     console.log("Google User", userInfo);
-    return { user: userInfo, tokens }
+    return {user: userInfo, tokens}
   } catch (error) {
     console.log("Error in google authentication", error);
-    return { error: "Failed to authenticate with google" }
+    return { error: "Failed to authenticate with google"}
   }
 }
 
